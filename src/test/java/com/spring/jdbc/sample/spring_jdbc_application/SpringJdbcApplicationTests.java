f7a099b708a23960dddb49b6cc74ca938f21bbda
Arun Gandhi <arungandhi@Aruns-MacBook-Air.local>
2024-09-07 19:51:46 +0530

    spring jdbc changes

---
+++ src/test/java/com/spring/jdbc/sample/spring_jdbc_application/SpringJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.jdbc.sample.spring_jdbc_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
